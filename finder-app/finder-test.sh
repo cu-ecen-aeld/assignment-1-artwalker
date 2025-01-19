421b03d25f343fa320d9a937d7fa00c32b23e761
Ethan Wang <xinxingwang@acm.org>
2025-01-19 12:40:56 +0800

    Update finder-test.sh for assigment4

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -9,7 +9,7 @@ NUMFILES=10
 WRITESTR=AELD_IS_FUN
 WRITEDIR=/tmp/aeld-data
 #username=$(cat conf/username.txt)
-username=$(cat /home/xinxingwang/Projects/Coursera/13_LinuxSystemProgramming/assignment-4-artwalker/conf/username.txt)
+username=$(cat /etc/finder-app/conf/username.txt)
 
 if [ $# -lt 3 ]
 then
@@ -35,7 +35,7 @@ rm -rf "${WRITEDIR}"
 # create $WRITEDIR if not assignment1
 # assignment=$(cat ../conf/assignment.txt)
 # assignment=$(cat conf/assignment.txt)
-assignment=$(cat /home/xinxingwang/Projects/Coursera/13_LinuxSystemProgramming/assignment-4-artwalker/conf/assignment.txt)
+assignment=$(cat /etc/finder-app/conf/assignment.txt)
 
 if [ $assignment != 'assignment1' ]
 then
